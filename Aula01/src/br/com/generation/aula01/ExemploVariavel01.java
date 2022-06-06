61ec13a61dac03b024d6fc64103f33421e9261d3
Matheus <matheuslsv7@gmail.com>
2022-06-06 18:03:56 -0300

    Importanto os exercicios de operadores logicos

---
+++ Aula01/src/br/com/generation/aula01/ExemploVariavel01.java
@@ -0,0 +1,21 @@
+package br.com.generation.aula01;
+
+public class ExemploVariavel01 {
+
+	public static void main(String[] args) {
+		
+		int var1;
+		int var2;
+		
+		var1 = 1000;
+		
+		System.out.println("Valor da variavel 1: " + var1);
+		//System.out.printf("Valor da variavel 1: %d", var1);
+		
+		var2 = var1 / 2;
+		
+		System.out.println("Valor da variavel 2: " + var2);
+
+	}
+
+}
---
+++ Aula01/src/br/com/generation/aula01/ExemploVariavel03.java
@@ -0,0 +1,18 @@
+package br.com.generation.aula01;
+
+public class ExemploVariavel03 {
+
+	public static void main(String[] args) {
+		
+		double galoes, litros;
+		
+		galoes = 10;
+		
+		litros = galoes * 3.7854;
+		
+		System.out.println(galoes + " Galões americanos são: " + litros + " Litros.");
+		
+
+	}
+
+}
